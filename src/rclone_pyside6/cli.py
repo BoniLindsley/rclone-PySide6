1654065ff43645978bb380d8855e8896eea1ff28
Boni Lindsley <boni@lindsley.xyz>
2022-12-21 01:11:14 +0000

    add: server start

--- src/rclone_pyside6/cli.py
+++ src/rclone_pyside6/cli.py
@@ -1,13 +1,159 @@
 #!/usr/bin/env python3
 
+# Standard libraries.
+import collections.abc
+import functools
+import logging
+import platform
+import secrets
+import subprocess
+import sys
+import types
+import typing
+
+
 # External dependencies.
 import click
+import click_repl  # type: ignore[import]
 
-# Internal packages.
-from . import greet
+_F = typing.TypeVar("_F", bound=collections.abc.Callable[..., typing.Any])
+_T = typing.TypeVar("_T")
+
+_logger = logging.getLogger(__name__)
 
 
-@click.command()
-@click.argument("names", nargs=-1)
-def group(names: list[str]) -> None:
-    greet.hello_all(*names, echo=click.echo)
+def kill_subprocesses(process: subprocess.Popen[bytes]) -> None:  # pragma: no cover
+    if platform.system() == "Windows":
+        # A way to kill all child subprocess in Windows.
+        subprocess.call(["taskkill", "/F", "/PID", str(process.pid), "/T"])
+    else:
+        # Not sure how to do this on other platforms.
+        process.kill()
+
+
+class RcdServer:
+    __Self = typing.TypeVar("__Self", bound="RcdServer")
+    meta_key = f"{__package__}.rcd_server"
+
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        super().__init__(*args, **kwargs)
+        self.realm = "rclone"
+        self.user = secrets.token_urlsafe()
+        self.password = secrets.token_urlsafe()
+        self.address = "localhost"
+        self.port = 5572
+        self.rcd_subprocess: subprocess.Popen[bytes] | None = None
+
+    def __enter__(self: __Self) -> __Self:
+        _logger.debug("Context object opening.")
+        return self
+
+    def __exit__(  # pylint: disable=useless-return
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: types.TracebackType | None,
+    ) -> bool | None:
+        del exc_type
+        del exc_value
+        del traceback
+        _logger.debug("Context object closing.")
+        self.stop()
+        return None
+
+    @classmethod
+    def pass_to_command(cls, command: _F) -> _F:
+        def decorated(*args: object, **kwargs: object) -> object:
+            context = click.get_current_context()
+            rcd_server = cls.from_context(context)
+            return command(rcd_server, *args, **kwargs)
+
+        return functools.update_wrapper(typing.cast(_F, decorated), command)
+
+    @classmethod
+    def from_context(cls: type[__Self], ctx: click.Context) -> __Self:
+        obj = ctx.meta.get(cls.meta_key)
+        if isinstance(obj, cls):
+            return obj
+
+        if obj is not None:
+            _logger.warning(
+                "Overwriting existing click context object of type %s", type(obj)
+            )
+        del obj
+
+        # Create and store the actual server object.
+        new_obj = ctx.meta[cls.meta_key] = cls()
+
+        # Assign cleanup responsibility to the out-most Click context.
+        # This is because the Click meta map is used by all contexts in the hierarchy.
+        root_ctx = ctx
+        parent_ctx: click.Context | None = ctx
+        while parent_ctx is not None:
+            root_ctx = parent_ctx
+            parent_ctx = parent_ctx.parent
+        root_ctx.with_resource(new_obj)
+        return new_obj
+
+    def start(self) -> None:
+        if self.rcd_subprocess is not None:
+            _logger.warning("Existing rclone rcd subprocess not properly stopped.")
+            return
+        arguments = self.create_rcd_command()
+        _logger.debug("Starting rclone rcd with: %s", arguments)
+        rcd_subprocess = subprocess.Popen(  # pylint: disable=consider-using-with
+            arguments
+        )
+        self.rcd_subprocess = rcd_subprocess
+
+    def stop(self) -> None:
+        _logger.debug("Requested to stop existing rclone rcd subprocess.")
+        rcd_subprocess = self.rcd_subprocess
+        if rcd_subprocess is None:
+            _logger.debug("No existing rclone rcd subprocess to stop.")
+            return
+        if rcd_subprocess.returncode is None:
+            _logger.info("Terminating existing rclone rcd subprocess.")
+            kill_subprocesses(rcd_subprocess)
+        rcd_subprocess.communicate()
+        _logger.debug(
+            "Return code of rclone rcd subprocess: %s", rcd_subprocess.returncode
+        )
+        self.rcd_subprocess = None
+
+    def create_rcd_command(self) -> list[str]:
+        return [
+            "rclone",
+            "rcd",
+            f"--rc-realm={self.realm}",
+            f"--rc-user={self.user}",
+            f"--rc-pass={self.password}",
+            f"--rc-addr={self.address}:{self.port}",
+        ]
+
+
+@click.group()
+def cli() -> None:
+    pass
+
+
+@cli.command()
+@RcdServer.pass_to_command
+def start(rcd_server: RcdServer) -> None:
+    rcd_server.start()
+
+
+@cli.command()
+@RcdServer.pass_to_command
+def stop(rcd_server: RcdServer) -> None:
+    rcd_server.stop()
+
+
+def main() -> int:  # pragma: no cover
+    click_repl.register_repl(cli)
+    cli()
+    return 0
+
+
+if __name__ == "__main__":  # pragma: no cover
+    sys.exit(main())
--- tests/test_rclone_pyside6/test_cli.py
+++ tests/test_rclone_pyside6/test_cli.py
@@ -1,23 +1,228 @@
 #!/usr/bin/env python3
 
+# Standard libraries.
+import collections.abc
+import subprocess
+import sys
+import typing
+
 # External dependencies.
 import click
 import click.testing
 
 # Internal modules
 import rclone_pyside6.cli
+from rclone_pyside6.cli import RcdServer
+
+_F = typing.TypeVar("_F", bound=collections.abc.Callable[..., typing.Any])
 
 
-def test_group_echoes_greetings() -> None:
-    group = rclone_pyside6.cli.group
+@click.command()
+def click_command() -> None:
+    pass
+
+
+class TestRcdServer:
+    def test_default_constructor(self) -> None:
+        RcdServer()
+
+    def test_context_manager_default_enter_and_exit(self) -> None:
+        with RcdServer() as rcd_server:
+            assert isinstance(rcd_server, RcdServer)
+
+    def test_context_manager_exit_kills_subprocess(self) -> None:
+        with RcdServer() as rcd_server:
+            process = subprocess.Popen(  # pylint: disable=consider-using-with
+                [sys.executable]
+            )
+            rcd_server.rcd_subprocess = process
+            assert process.returncode is None
+        assert process.returncode is not None
+
+    def test_pass_to_command_forwards_server_instance(self) -> None:
+        @click.command()
+        @RcdServer.pass_to_command
+        def command(rcd_server: RcdServer) -> None:
+            assert isinstance(rcd_server, RcdServer)
+            click.echo("test_pass_to_command_forwards_server_instance")
+
+        runner = click.testing.CliRunner(mix_stderr=False)
+        result = runner.invoke(command)
+        assert result.stderr == ""
+        assert result.stdout == "test_pass_to_command_forwards_server_instance\n"
+        assert result.exception is None
+        assert result.exit_code == 0
+
+    def test_from_context_creates_server_if_missing(self) -> None:
+        with click.Context(click_command) as ctx:
+            rcd_server = RcdServer.from_context(ctx)
+            assert isinstance(rcd_server, RcdServer)
+
+    def test_from_context_ensures_server_cleanup(self) -> None:
+        rcd_server_dict: list[RcdServer] = []
+        rcd_subprocess_dict: list[subprocess.Popen[bytes]] = []
+
+        @click.command()
+        def command() -> None:
+            ctx = click.get_current_context()
+            rcd_server_dict.append(RcdServer.from_context(ctx))
+            assert rcd_server_dict[0].rcd_subprocess is None
+
+            rcd_server_dict[0].start()
+            rcd_subprocess = rcd_server_dict[0].rcd_subprocess
+            assert rcd_subprocess is not None
+            rcd_subprocess_dict.append(rcd_subprocess)
+            assert rcd_subprocess.returncode is None
+
+        runner = click.testing.CliRunner(mix_stderr=False)
+        result = runner.invoke(command, input="\n")
+        assert result.stderr == ""
+        assert result.stdout == ""
+        assert result.exception is None
+        assert result.exit_code == 0
+
+        assert rcd_server_dict[0].rcd_subprocess is None
+        assert rcd_subprocess_dict[0].returncode is not None
+
+    def test_from_context_attaches_resource_to_root_context(self) -> None:
+        rcd_server_dict: list[RcdServer] = []
+        rcd_subprocess_dict: list[subprocess.Popen[bytes]] = []
+
+        @click.command()
+        def command() -> None:
+            root = click.get_current_context()
+            with click.Context(click_command, parent=root) as ctx:
+                rcd_server_dict.append(RcdServer.from_context(ctx))
+                assert rcd_server_dict[0].rcd_subprocess is None
+
+                rcd_server_dict[0].start()
+                rcd_subprocess = rcd_server_dict[0].rcd_subprocess
+                assert rcd_subprocess is not None
+                rcd_subprocess_dict.append(rcd_subprocess)
+                assert rcd_subprocess.returncode is None
+            assert rcd_subprocess_dict[0].returncode is None
+
+        runner = click.testing.CliRunner(mix_stderr=False)
+        result = runner.invoke(command, input="\n")
+        assert result.stderr == ""
+        assert result.stdout == ""
+        assert result.exception is None
+        assert result.exit_code == 0
+
+        assert rcd_server_dict[0].rcd_subprocess is None
+        assert rcd_subprocess_dict[0].returncode is not None
+
+    def test_from_context_reuses_server_if_exists(self) -> None:
+        with click.Context(click_command) as ctx:
+            rcd_server = RcdServer.from_context(ctx)
+            rcd_server_2 = RcdServer.from_context(ctx)
+            assert rcd_server is rcd_server_2
+
+    def test_from_context_overwrites_metakey_if_not_server(self) -> None:
+        with click.Context(click_command) as ctx:
+            ctx.meta[RcdServer.meta_key] = 1
+            rcd_server = RcdServer.from_context(ctx)
+            assert isinstance(rcd_server, RcdServer)
+
+    def test_start_and_stop(self) -> None:
+        rcd_server = RcdServer()
+        assert rcd_server.rcd_subprocess is None
+
+        rcd_server.start()
+        rcd_subprocess = rcd_server.rcd_subprocess
+        assert isinstance(rcd_subprocess, subprocess.Popen)
+        with rcd_subprocess:
+            assert rcd_subprocess.returncode is None
+
+            rcd_server.stop()
+            assert rcd_server.rcd_subprocess is None
+            assert rcd_subprocess.returncode is not None
+
+    def test_start_ignores_restart(self) -> None:
+        rcd_server = RcdServer()
+        assert rcd_server.rcd_subprocess is None
+
+        rcd_server.start()
+        rcd_subprocess = rcd_server.rcd_subprocess
+        assert isinstance(rcd_subprocess, subprocess.Popen)
+        with rcd_subprocess:
+            assert rcd_subprocess.returncode is None
+
+            rcd_server.start()
+            rcd_subprocess_2 = rcd_server.rcd_subprocess
+            assert rcd_subprocess is rcd_subprocess_2
+
+    def test_stop_is_okay_if_not_started(self) -> None:
+        rcd_server = RcdServer()
+        assert rcd_server.rcd_subprocess is None
+        rcd_server.stop()
+        assert rcd_server.rcd_subprocess is None
+
+    def test_stop_is_okay_if_subprocess_stops_unexpectedly(self) -> None:
+        rcd_server = RcdServer()
+        assert rcd_server.rcd_subprocess is None
+
+        rcd_server.start()
+        rcd_subprocess = rcd_server.rcd_subprocess
+        assert isinstance(rcd_subprocess, subprocess.Popen)
+        with rcd_subprocess:
+            assert rcd_subprocess.returncode is None
+            rclone_pyside6.cli.kill_subprocesses(rcd_subprocess)
+            rcd_subprocess.communicate()
+            assert rcd_subprocess.returncode is not None
+
+            assert rcd_server.rcd_subprocess is not None
+            rcd_server.stop()
+            assert rcd_server.rcd_subprocess is None
+
+    def test_create_rcd_command_gives_rclone_command(self) -> None:
+        rcd_server = RcdServer()
+        rcd_server.address = "address"
+        rcd_server.password = "password"
+        rcd_server.port = 1234
+        rcd_server.realm = "realm"
+        rcd_server.user = "user"
+        command = rcd_server.create_rcd_command()
+        assert len(command) == 6
+        assert command[0] == "rclone"
+        assert command[1] == "rcd"
+        assert "--rc-realm=realm" in command
+        assert "--rc-user=user" in command
+        assert "--rc-pass=password" in command
+        assert "--rc-addr=address:1234" in command
+
+
+def test_cli_default_is_help() -> None:
     runner = click.testing.CliRunner(mix_stderr=False)
-    result = runner.invoke(group, ("Alpha", "Beta", "", "Gamma", "Delta"))
+    result = runner.invoke(rclone_pyside6.cli.cli)
     assert result.stderr == ""
-    assert result.stdout.splitlines() == [
-        "Hello, Alpha!",
-        "Hello, Beta!",
-        "Hello, Gamma!",
-        "Hello, Delta!",
-    ]
+    assert result.stdout != ""
+    assert result.exception is None
+    assert result.exit_code == 0
+
+
+def test_cli_has_stop_subcommand() -> None:
+    runner = click.testing.CliRunner(mix_stderr=False)
+    result = runner.invoke(rclone_pyside6.cli.cli, ["stop"])
+    assert result.stderr == ""
+    assert result.stdout == ""
+    assert result.exception is None
+    assert result.exit_code == 0
+
+
+def test_start_callable() -> None:
+    runner = click.testing.CliRunner(mix_stderr=False)
+    result = runner.invoke(rclone_pyside6.cli.start)
+    assert result.stderr == ""
+    assert result.stdout == ""
+    assert result.exception is None
+    assert result.exit_code == 0
+
+
+def test_stop_callable() -> None:
+    runner = click.testing.CliRunner(mix_stderr=False)
+    result = runner.invoke(rclone_pyside6.cli.stop)
+    assert result.stderr == ""
+    assert result.stdout == ""
     assert result.exception is None
     assert result.exit_code == 0
